07283d4c53840c85db0879996feb4adc3c8002ac
Caio Mateus <caiomateusgames@outlook.com>
2019-07-04 12:40:02 -0300

    Adicionando o template das categorias.

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -4,8 +4,8 @@ import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 
 const routes: Routes = [
-  { path: 'categories', component: CategoryListComponent },
-  { path: 'new', component: CategoryFormComponent}, 
+  { path: '', component: CategoryListComponent },
+  { path: 'new', component: CategoryFormComponent},
   { path: ':id/edit', component: CategoryFormComponent}
 ];
 
